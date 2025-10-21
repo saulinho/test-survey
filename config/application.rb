aa56ed75de0ff9d96970dc931f4a3562ec0f8767
saulinho <saulomorais@saulinho.localdomain>
2025-10-21 16:52:16 -0300

    Add multi tenant

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module TestSurvey
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "brasilia"
+    config.time_zone = "Brasilia"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts = nil
   end
 end
